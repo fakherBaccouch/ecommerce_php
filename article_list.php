5144b9486b95f9a697a31ebe67027f2ac7558cde
FakherBaccouch <bakouchfakher@gmail.com>
2020-12-09 03:09:36 +0100

    panier

--- article_list.php
+++ article_list.php
@@ -9,8 +9,9 @@ $pdo = pdo_connect_mysql();
 // Get the page via GET request (URL param: page), if non exists default the page to 1
 $page = isset($_GET['page']) && is_numeric($_GET['page']) ? (int)$_GET['page'] : 1;
 // Number of records to show on each page
-$records_per_page = 5;
+$records_per_page = 9;
 // Prepare the SQL statement and get records from our contacts table, LIMIT will determine the page
+
 $stmt = $pdo->prepare('SELECT * FROM Articles ORDER BY id LIMIT :current_page, :record_per_page');
 $stmt->bindValue(':current_page', ($page-1)*$records_per_page, PDO::PARAM_INT);
 $stmt->bindValue(':record_per_page', $records_per_page, PDO::PARAM_INT);
@@ -20,7 +21,8 @@ $Articles = $stmt->fetchAll(PDO::FETCH_ASSOC);
 // Get the total number of contacts, this is so we can determine whether there should be a next and previous c
 $num_contacts = $pdo->query('SELECT COUNT(*) FROM Articles')->fetchColumn();
 ?>
-  <div style="display:flex">
+  <div style="display:grid;  grid-template-columns: auto auto auto;
+">
     <?php foreach ($Articles as $Article): ?>
 
 <div class="product-container">
--- panier.php
+++ panier.php
@@ -29,13 +29,86 @@ try {
   $sql = "INSERT INTO panier (id, libelle, image,prix,categorie,promo)
   VALUES ($id, $libelle, $image,$prix,$categorie,$promo)";
   $conn->exec($sql);
-  echo "New record created successfully";
   $stmt = $conn->prepare("SELECT * FROM panier ");
   $stmt->execute();
 
   // set the resulting array to associative
   $Articles = $stmt->fetchAll(PDO::FETCH_ASSOC);
-echo $Articles[3]["id"];
+?>
+
+
+
+<div id="panier">
+<div class="left-container">
+    <table style="width:100%">
+    <tr>
+    <th >Image</th>
+    <th>Libelle</th>
+    <th>Prix</th>
+    <th>Qte</th>
+    <th>Categorie</th>
+
+
+
+  </tr>
+  <?php foreach ($Articles as $Article): ?>
+<tr>
+  <td >  <img class="img-panier" src="<?php echo $Article['image']?>"></td>
+  <td> <?php echo $Article["libelle"]?> </td>
+  <td> <?php echo $Article["prix"]?> </td>
+  <td> <label for="tentacles"></label>
+
+<input type="number" id="tentacles" name="tentacles"
+       min="10" max="100"> </td>
+  <td> <?php echo $Article["categorie"]?> </td>
+
+  </tr>
+<?php endforeach; ?>
+    </table>
+
+</div>
+
+     <div class="right-container">
+         <div class="right-container-items">
+             <h1>ORDER SUMMARY</h1>
+             <br>
+             <hr>
+             <br>
+          <div style="display:flex;justify-content:space-between"><h4>ITEMS:</h4> <li style="list-style-type:none">50</li></div>   
+             <br>
+             <div style="display:flex;justify-content:space-between"><h4>TOTAL:</h4> <li style="list-style-type:none">$550</li></div>   
+             
+             <button class="button button5">Checkout</button>
+
+         </div>
+     </div>
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
 } catch(PDOException $e) {
   echo $sql . "<br>" . $e->getMessage();
 }
